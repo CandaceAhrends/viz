11814020101710cc2d52e55b605a1d07bf0bd1e6
Candace Ahrends <candaceahrends@Candaces-MacBook-Pro.local>
2024-11-24 17:19:12 -0500

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  singleQuote: true, // Enforce single quotes
+};
---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        'golos-text': ['Golos Text', 'sans-serif'],
+      },
+      fontSize: {
+        xs: '0.8rem',
+        xm: '1.2rem',
+        xxl: '2.2rem',
+      },
+      colors: {
+        'black-900': '#202020',
+        'neutral-600': '#1F1F1F',
+        'neutral-900': '#050505',
+        'neutral-400': '#999999',
+        'brand-blue': '#00BDDD',
+        green: '#0FEDBE',
+      },
+    },
+  },
+  plugins: [],
+};

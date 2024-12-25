55353b66f84e1449db49a72c742637c8ef93d439
Candace Ahrends <candaceahrends@Candaces-MacBook-Pro.local>
2024-12-25 15:01:22 -0500

    save work

--- src/components/content/Header.jsx
+++ src/components/content/Header.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import SideMenu from '../navigation/SideMenu';
+import { StockContext } from '../../StockContext';
 import HamIconToggle from './HamIconToggle';
 
 const Header = () => {
+  const { selectedStock } = useContext(StockContext);
   return (
     <header className="text-white h-[40px]  mt-1  border-b border-neutral-900 ">
-      <div className="flex justify-end w-full p-1 mr-5 mt-[.1rem] z-[999999]">
+      <p className="flex justify-center text-green ">{selectedStock}</p>
+      <div className="flex justify-end w-full p-1 mr-5 mt-[-1.2rem] z-[999999]">
         <HamIconToggle Menu={SideMenu} />
       </div>
     </header>
